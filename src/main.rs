4576fa9c06a295cc7ff350168c725197ad43631a
Vishakh Sandwar <vishakhs74@gmail.com>
2024-03-23 15:17:09 -0400

    debug: softmax blowup

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,25 @@ use rand::rngs::ThreadRng;
 use rand::thread_rng;
 use rand::Rng;
 
+fn argmax(matrix: Array2<f32>) -> Array1<f32> {
+    let mut output = Array1::zeros(matrix.shape()[1]);
+    for (i, row) in matrix.axis_iter(Axis(0)).enumerate() {
+        let (max_idx, max_val) =
+            row.iter()
+                .enumerate()
+                .fold((0, row[0]), |(idx_max, val_max), (idx, val)| {
+                    if &val_max > val {
+                        (idx_max, val_max)
+                    } else {
+                        (idx, *val)
+                    }
+                });
+        output[i] = max_idx as f32;
+    }
+    output
+}
+
+
 fn read_csv() -> DataFrame {
     // Read a CSV file into a DataFrame
     let df: DataFrame = CsvReader::from_path("train.csv")
@@ -20,7 +39,7 @@ fn read_csv() -> DataFrame {
 }
 
 fn parse_data(df: DataFrame) -> (Array2<f32>, usize, usize) {
-    let ndarray: Array2<f32> = df.to_ndarray::<Float32Type>(IndexOrder::C).unwrap();
+    let ndarray: Array2<f32> = df.to_ndarray::<Float32Type>(IndexOrder::Fortran).unwrap();
 
     let shape: &[usize] = ndarray.shape();
     let m: usize = shape[0];
@@ -57,13 +76,15 @@ fn split_data(ndarray: Array2<f32>) -> (Array2<f32>, Array1<f32>, Array2<f32>, A
     // Split data into training and testing sets
     let _pre_t = &ndarray.slice(s![0..1000, ..]);
     let data_dev = _pre_t.t();
-    let mut y_dev = &data_dev.slice(s![0, ..]);
-    let mut x_dev = &data_dev.slice(s![1.., ..]);
+    let y_dev = &data_dev.slice(s![0, ..]);
+    let x_dev = data_dev.slice(s![1.., ..]); // Remove the & before mut x_dev
+    let x_dev = x_dev.mapv(|x: f32| x / 255.0);
 
     let _pre_t = &ndarray.slice(s![1000.., ..]);
     let data_train = _pre_t.t();
-    let mut y_train = &data_train.slice(s![0, ..]);
-    let mut x_train = &data_train.slice(s![1.., ..]);
+    let y_train = &data_train.slice(s![0, ..]);
+    let x_train = data_train.slice(s![1.., ..]); 
+    let x_train = x_train.mapv(|x: f32| x / 255.0);
     let m_train = x_train.shape()[1];   
 
 
@@ -92,7 +113,10 @@ fn init_params() -> (Array2<f32>, Array1<f32>, Array2<f32>, Array1<f32>) {
     for elem in b2.iter_mut() {
         *elem = rng.gen::<f32>() - 0.5;
     }
-
+    // println!("w1: {:?}", w1);
+    // println!("b1: {:?}", b1);
+    // println!("w2: {:?}", w2);
+    // println!("b2: {:?}", b2);
     (w1, b1, w2, b2)
 }
 
@@ -100,10 +124,13 @@ fn re_lu(mut z: &Array2<f32>) -> Array2<f32> {
     return z.mapv(|x| x.max(0.0));
 }
 
+// TO DO: Fix the softmax function
+
 fn softmax(mut z: &Array2<f32>) -> Array2<f32> {
-    let tot = z.mapv(|x| x.exp()).sum();
-    let a: Array2<f32> = z.mapv(|x| x.exp() / tot);
-    return a
+    let tot = z.mapv(|x| x.exp());
+    // println!("{:?}", tot);
+    // let a: Array2<f32> = z.mapv(|x| x.exp() / tot);
+    return tot;
 }
 
 fn forward_prop(w1: &Array2<f32>, b1: &Array1<f32>, w2: &Array2<f32>, b2: &Array1<f32>, x: &mut Array2<f32>) -> (Array2<f32>, Array2<f32>, Array2<f32>, Array2<f32>) {
@@ -111,6 +138,18 @@ fn forward_prop(w1: &Array2<f32>, b1: &Array1<f32>, w2: &Array2<f32>, b2: &Array
     let a1: Array2<f32> = re_lu(&z1);
     let z2: Array2<f32> = w2.dot(&a1) + b2.broadcast((a1.shape()[1], 10)).unwrap().t();
     let a2: Array2<f32> = softmax(&z2);
+    // println!("x: {:?}", x);
+    // println!("w1: {:?}", w1);
+    // println!("x_1: {:?}", x.slice_axis(Axis(1), Slice::new(0,Some(1),1)));
+    // println!("w1_1: {:?}", w1.slice_axis(Axis(0), Slice::new(0,Some(1),1)));
+    // let ax = w1.slice_axis(Axis(0), Slice::new(0,Some(1),1)).to_owned().dot(&x.slice_axis(Axis(1), Slice::new(0,Some(1),1)).to_owned());
+    // println!("dot: {:?}",ax);
+    // println!("w1: {:?}", w1.dot(x));
+    // println!("b1: {:?}", b1.broadcast((x.shape()[1], 10)).unwrap().t());
+    // println!("z1: {:?}", z1);
+    // println!("a1: {:?}", a1);
+    // println!("z2: {:?}", z2);
+    // println!("a2: {:?}", a2);
     return (z1, a1, z2, a2)
 }
 
@@ -172,19 +211,8 @@ fn update_params(
 }
 
 fn get_predictions(a2: Array2<f32>) -> Array1<f32> {
-    let mut predictions: Array1<f32> = Array1::<f32>::zeros(a2.shape()[1]);
-    for i in 0..a2.shape()[1] {
-        let mut max_val: f32 = f32::NEG_INFINITY;
-        let mut max_idx: f32 = 0.0;
-        for j in 0..a2.shape()[0] {
-            if a2[[j, i]] > max_val {
-                max_val = a2[[j, i]];
-                max_idx = j as f32;
-            }
-        }
-        predictions[i] = max_idx;
-    }
-    predictions
+    println!("{:?}", argmax(a2));
+    return argmax(a2);
 }
 
 fn get_accuracy(predictions: Array1<f32>, y: &Array1<f32>) -> f32 {
@@ -200,8 +228,12 @@ fn get_accuracy(predictions: Array1<f32>, y: &Array1<f32>) -> f32 {
 fn gradient_descent(mut x: Array2<f32>, y: Array1<f32>, alpha: f32, iterations: i32) -> (Array2<f32>, Array1<f32>, Array2<f32>, Array1<f32>){
     let (mut w1, mut b1, mut w2, mut b2) = init_params();
     for i in 0..iterations {
-        let (z1, a1, _z2, a2) = forward_prop(&w1, &b1, &w2, &b2, &mut x);
-
+        println!("Iteration: {}", i);
+        let (z1, a1, z2, a2) = forward_prop(&w1, &b1, &w2, &b2, &mut x);
+        // println!("z1 {:?}", z1);
+        // println!("z2 {:?}", z2);
+        // println!("a1 {:?}", a1);
+        // println!("a2 {:?}", a2);
         let (dw1, db1, dw2, db2) = backward_prop(&z1, &a1, &a2, &w2, &x, &y, x.shape()[1]);
         (w1, b1, w2, b2) = update_params(w1, b1, w2, b2, dw1, db1, dw2, db2, alpha);
         if i % 10 == 0 {
